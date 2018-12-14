0577d9d759b21c5b242c55e41b82a8740c7e848b
nijiaqi <583522219@qq.com>
2018-12-14 18:09:05 +0800

    修改视图解析器

--- general-start/src/main/java/com/njq/start/config/BaseConfig.java
+++ general-start/src/main/java/com/njq/start/config/BaseConfig.java
@@ -1,5 +1,6 @@
 package com.njq.start.config;
 
+import com.njq.start.filter.ServletContextInit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
@@ -13,91 +14,140 @@ import org.thymeleaf.spring4.view.ThymeleafView;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
-import com.njq.start.filter.ServletContextInit;
 
 @Configuration
 public class BaseConfig {
-	// 视图前缀
-	private static final String VIEW_PREFIX = "/WEB-INF/jsp/";
-	// 视图后缀
-	private static final String VIEW_SUFFIX = ".jsp";
-	// 视图的内容类型。
-	private static final String VIEW_CONTENT_TYPE = "text/html;charset=UTF-8";
+    /**
+     * 视图的内容类型
+     */
+    private static final String VIEW_CONTENT_TYPE = "text/html;charset=UTF-8";
 
-	/**
-	 * 配置 视图解析器
-	 * 
-	 * @return
-	 */
-	@Bean
-	public ViewResolver jspviewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setCache(false);
-		resolver.setPrefix(VIEW_PREFIX);
-		resolver.setSuffix(VIEW_SUFFIX);
-		resolver.setExposeContextBeansAsAttributes(true);
-		resolver.setContentType(VIEW_CONTENT_TYPE);
-		resolver.setViewClass(JstlView.class);
-		resolver.setOrder(3);
-		return resolver;
-	}
+    /**
+     * 配置标准的html解析器
+     *
+     * @return
+     */
+    @Bean
+    public TemplateResolver templateResolver() {
+        TemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        //视图解析器的使用优先级，越小优先级越高
+        templateResolver.setOrder(0);
+        //是否使用视图缓存，开发时不建议启用缓存，改动即可生效
+        templateResolver.setCacheable(false);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
 
-	@Bean
-	public FreeMarkerConfigurer ftlconfigure() {
-		FreeMarkerConfigurer configure = new FreeMarkerConfigurer();
-		configure.setTemplateLoaderPath("/WEB-INF/ftl/");
-		configure.setDefaultEncoding("utf-8");
-		return configure;
-	}
+    /**
+     * 生成标准解析器的模板引擎
+     *
+     * @return
+     */
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
 
-	@Bean
-	public ViewResolver ftlViewResolver() {
-		FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
-		resolver.setCache(false);
-		resolver.setSuffix(".ftl");
-		resolver.setExposeContextBeansAsAttributes(true);
-		resolver.setContentType(VIEW_CONTENT_TYPE);
-		resolver.setViewNames("freemarker");
-		resolver.setViewClass(FreeMarkerView.class);
-		resolver.setOrder(2);
-		return resolver;
-	}
+    /**
+     * 配置thymeleaf解析器，
+     * 它得基于标准的解析器模板引擎
+     *
+     * @return
+     */
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(templateEngine());
+        thymeleafViewResolver.setViewClass(ThymeleafView.class);
+        /*
+         * 他会根据视图名字开头来决定使用哪一类视图解析器
+         * 返回视图名字（controller里面return的以这个开头，如return “thymeleaf/thytest”），
+         * 返回的视图全路径为Prefix+“thymeleaf/thytest”
+         * 比如此视图路径为：“/WEB-INF/thymeleaf/thytest”
+         */
+        thymeleafViewResolver.setViewNames(new String[]{"thymeleaf/*"});
+        thymeleafViewResolver.setCharacterEncoding("UTF-8");
+        //视图解析器的使用优先级，越小优先级越高
+        thymeleafViewResolver.setOrder(1);
+        //是否使用视图缓存，开发时不启用缓存，改动即可生效
+        thymeleafViewResolver.setCache(false);
+        return thymeleafViewResolver;
+    }
 
-	@Bean
-	public TemplateResolver templateResolver() {
-		TemplateResolver templateResolver = new ServletContextTemplateResolver();
-		templateResolver.setPrefix("/WEB-INF/thymeleaf/");
-		templateResolver.setSuffix(".html");
-		templateResolver.setTemplateMode("HTML5");
-		templateResolver.setOrder(0);
-		templateResolver.setCacheable(false);
-		templateResolver.setCharacterEncoding("UTF-8");
-		return templateResolver;
-	}
+//freemark视图解析器--------------------------------------------------------------
 
-	@Bean
-	public SpringTemplateEngine templateEngine() {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(templateResolver());
-		return templateEngine;
-	}
+    /**
+     * 生成freemark的配置类，不配置加载freemark解析器会报错
+     *
+     * @return
+     */
+    @Bean
+    public FreeMarkerConfigurer ftlconfigure() {
+        FreeMarkerConfigurer configure = new FreeMarkerConfigurer();
+        configure.setTemplateLoaderPath("/WEB-INF/");
+        configure.setDefaultEncoding("utf-8");
+        return configure;
+    }
 
-	@Bean
-	public ThymeleafViewResolver thymeleafViewResolver() {
-		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
-		resolver.setTemplateEngine(templateEngine());
-		resolver.setViewNames(new String[]{"thymeleaf/*"});
-		resolver.setViewClass(ThymeleafView.class);
-		resolver.setCharacterEncoding("UTF-8");
-		resolver.setContentType(VIEW_CONTENT_TYPE);
-		resolver.setCache(false);
-		resolver.setOrder(1);
-		return resolver;
-	}
+    /**
+     * 生成freemark的视图解析器
+     *
+     * @return
+     */
+    @Bean
+    public ViewResolver ftlViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(false);
+        resolver.setSuffix(".ftl");
+        resolver.setExposeContextBeansAsAttributes(true);
+        resolver.setContentType(VIEW_CONTENT_TYPE);
+        /*
+         * 他会根据视图名字开头来决定使用哪一类视图解析器
+         * 返回视图名字（controller里面return的以这个开头，如return “ftl/freetest”），
+         * 返回的视图全路径为Prefix+“ftl/freetest”，注意：Prefix只要填/WEB-INF/即可
+         * 比如此视图路径为：“/WEB-INF/ftl/freetest”
+         * 视图解析器是在dispatcherservlet中的resolveViewName方法进行解析
+         */
+        resolver.setViewNames("ftl/*");
+        resolver.setViewClass(FreeMarkerView.class);
+        //视图解析器的使用优先级，越小优先级越高
+        resolver.setOrder(2);
+        return resolver;
+    }
+//jsp视图解析器--------------------------------------------------------------
 
-	@Bean
-	public ServletContextInit servletContextInit() {
-		return new ServletContextInit();
-	}
+    /**
+     * 配置jsp视图解析器
+     *
+     * @return
+     */
+    @Bean
+    public ViewResolver jspviewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setCache(false);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        resolver.setContentType(VIEW_CONTENT_TYPE);
+        resolver.setViewClass(JstlView.class);
+        //不填视图解析名字，说明除了上面的全用此解析器
+//		resolver.setViewNames("jsp/*");
+        //视图解析器的使用优先级，越小优先级越高
+        resolver.setOrder(3);
+        return resolver;
+    }
+
+    /**
+     * 初始化系统的常用常量
+     */
+    @Bean
+    public ServletContextInit servletContextInit() {
+        return new ServletContextInit();
+    }
 
 }
--- general-start/src/main/java/com/njq/start/filter/ServletContextInit.java
+++ general-start/src/main/java/com/njq/start/filter/ServletContextInit.java
@@ -1,44 +1,46 @@
 package com.njq.start.filter;
 
-import javax.servlet.ServletContext;
-
+import com.njq.common.base.redis.RedisCommon;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.context.ServletContextAware;
 
-import com.njq.common.base.redis.RedisCommon;
+import javax.servlet.ServletContext;
 
 public class ServletContextInit implements ServletContextAware,
-		InitializingBean {
-	private ServletContext sc;
-	@Autowired
-	private StringRedisTemplate redisTemplate;
-	@Value("${image.url}")
-	private String imageUrl;
-	@Value("${auth.url}")
-	private String authUrl;
-	public void afterPropertiesSet() throws Exception {
-		String path = sc.getContextPath();
+        InitializingBean {
+    private ServletContext sc;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    @Value("${image.url}")
+    private String imageUrl;
+    @Value("${auth.url}")
+    private String authUrl;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        String path = sc.getContextPath();
 //		PropertyUtil.init(sc);
-		sc.setAttribute("path", path);
-		sc.setAttribute("resPath", path+"/static");
-		sc.setAttribute("imgPath", imageUrl);
-		sc.setAttribute("authUrl", authUrl);
-		RedisCommon.redisTemplate=this.redisTemplate;
-	}
+        sc.setAttribute("path", path);
+        sc.setAttribute("resPath", path + "/static");
+        sc.setAttribute("imgPath", imageUrl);
+        sc.setAttribute("authUrl", authUrl);
+        RedisCommon.redisTemplate = this.redisTemplate;
+    }
 
-	public void setServletContext(ServletContext servletContext) {
-		this.sc = servletContext;
-	}
-	
-	public StringRedisTemplate getRedisTemplate() {
-		return redisTemplate;
-	}
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.sc = servletContext;
+    }
 
-	public void setRedisTemplate(StringRedisTemplate redisTemplate) {
-		this.redisTemplate = redisTemplate;
-	}
+    public StringRedisTemplate getRedisTemplate() {
+        return redisTemplate;
+    }
+
+    public void setRedisTemplate(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
 }
