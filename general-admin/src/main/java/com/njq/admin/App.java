c79a6b6126425c1336bd04dbfe34718fb509a0ca
nijiaqi <583522219@qq.com>
2018-12-21 10:40:59 +0800

    调整

--- general-admin/src/main/java/com/njq/admin/App.java
+++ general-admin/src/main/java/com/njq/admin/App.java
@@ -1,16 +1,11 @@
 package com.njq.admin;
 
-import com.njq.common.util.date.TestUtil;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	TestUtil.getVAlue();
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
