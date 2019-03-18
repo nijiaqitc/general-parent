6fcae6635a307d92f047b16768fa752522d5ca1e
nijiaqi <583522219@qq.com>
2019-03-18 14:21:07 +0800

    枷锁

--- general-basis/src/main/java/com/njq/basis/service/impl/BaseTipService.java
+++ general-basis/src/main/java/com/njq/basis/service/impl/BaseTipService.java
@@ -1,18 +1,5 @@
 package com.njq.basis.service.impl;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.annotation.Resource;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
 import com.njq.common.base.constants.TitleType;
 import com.njq.common.base.dao.ConditionsCommon;
 import com.njq.common.base.dao.DaoCommon;
@@ -24,6 +11,17 @@ import com.njq.common.model.po.BaseTip;
 import com.njq.common.model.po.BaseTipConfig;
 import com.njq.common.model.vo.LabelNameVO;
 import com.njq.common.util.string.StringUtil2;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BaseTipService {
@@ -35,6 +33,7 @@ public class BaseTipService {
     private DaoCommon<BaseTipConfig> baseTipConfigDao;
     @Resource
     private JedisLockFactory jedisLockFactory;
+
     /**
      * 校验是否存在标签，没有则进行保存
      *
@@ -42,31 +41,34 @@ public class BaseTipService {
      * @return
      */
     public Long checkAndSave(String tipName) {
-    	String lockKey = StringUtil2.format("tipName-{0}", tipName);
-    	BaseTip tip = null ;
-    	try (JedisLock jedisLock = this.jedisLockFactory.getLock(lockKey)) {
-    		if (!jedisLock.acquire()) {
+        String lockKey = StringUtil2.format("tipName-{0}", tipName);
+        BaseTip tip;
+        try (JedisLock jedisLock = this.jedisLockFactory.getLock(lockKey)) {
+            if (!jedisLock.acquire()) {
                 throw new BaseKnownException("tip保存并发获取锁失败！");
             }
-    		ConditionsCommon condition = new ConditionsCommon();
-    		condition.addEqParam("tipName", tipName);
-    		tip = baseTipDao.queryTByParamForOne(condition);
-    		if (tip == null) {
-    			tip = new BaseTip();
-    			tip.setCreateDate(new Date());
-    			tip.setTipName(tipName);
-    			baseTipDao.save(tip);
-    		}
-		} catch (Exception e) {
-			try {
-				Thread.sleep(2000);
-				this.checkAndSave(tipName);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-		}
-    	return tip==null?null:tip.getId();
+            ConditionsCommon condition = new ConditionsCommon();
+            condition.addEqParam("tipName", tipName);
+            tip = baseTipDao.queryTByParamForOne(condition);
+            if (tip == null) {
+                tip = new BaseTip();
+                tip.setCreateDate(new Date());
+                tip.setTipName(tipName);
+                baseTipDao.save(tip);
+            }
+            return tip.getId();
+        } catch (BaseKnownException e1) {
+            try {
+                Thread.sleep(2000);
+                return this.checkAndSave(tipName);
+            } catch (InterruptedException e2) {
+                throw new RuntimeException(e2.getMessage());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
+
 
     /**
      * 保存批量标签（标签之间用,分割）
@@ -91,15 +93,15 @@ public class BaseTipService {
     }
 
     public BaseTip getById(Long id) {
-    	return baseTipDao.queryTById(id);
+        return baseTipDao.queryTById(id);
     }
-    
+
     public BaseTip getByName(String tipName) {
-    	ConditionsCommon condition = new ConditionsCommon();
-    	condition.addEqParam("tip_name", tipName);
-    	return baseTipDao.queryTByParamForOne(condition);
+        ConditionsCommon condition = new ConditionsCommon();
+        condition.addEqParam("tip_name", tipName);
+        return baseTipDao.queryTByParamForOne(condition);
     }
-    
+
     public List<BaseTip> getTipListByIds(String ids) {
         String[] tipIds = ids.split(",");
         List<BaseTip> list = new ArrayList<>();
@@ -116,61 +118,83 @@ public class BaseTipService {
         }
         String tips[] = tipIds.split(",");
         for (int i = 0; i < tips.length; i++) {
-        	ConditionsCommon con=new ConditionsCommon();
-        	con.addEqParam("tipId", Long.valueOf(tips[i]));
-        	con.addEqParam("titleId", titleId);
-        	List<BaseTipConfig> ll = baseTipConfigDao.queryColumnForList(con);
-        	if(CollectionUtils.isEmpty(ll)) {
-        		BaseTipConfig config = new BaseTipConfig();
-        		config.setCreateDate(new Date());
-        		config.setTipId(Long.valueOf(tips[i]));
-        		config.setSourceType(type.getValue());
-        		config.setTitleId(titleId);
-        		baseTipConfigDao.save(config);
-        	}
+            String lockKey = StringUtil2.format("addtipNum-{0}-{1}", tips[i], titleId);
+            try (JedisLock jedisLock = this.jedisLockFactory.getLock(lockKey)) {
+                if (!jedisLock.acquire()) {
+                    throw new BaseKnownException("tip保存并发获取锁失败！");
+                }
+                ConditionsCommon con = new ConditionsCommon();
+                con.addEqParam("tipId", Long.valueOf(tips[i]));
+                con.addEqParam("titleId", titleId);
+                List<BaseTipConfig> ll = baseTipConfigDao.queryColumnForList(con);
+                if (CollectionUtils.isEmpty(ll)) {
+                    BaseTipConfig config = new BaseTipConfig();
+                    config.setCreateDate(new Date());
+                    config.setTipId(Long.valueOf(tips[i]));
+                    config.setSourceType(type.getValue());
+                    config.setTitleId(titleId);
+                    baseTipConfigDao.save(config);
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
         }
     }
 
 
     public void saveToRepairTip(BaseTipConfig config) {
-    	baseTipConfigDao.save(config);
+        String lockKey = StringUtil2.format("repairtipNum-{0}-{1}", config.getTipId(), config.getTitleId());
+        try (JedisLock jedisLock = this.jedisLockFactory.getLock(lockKey)) {
+            if (!jedisLock.acquire()) {
+                throw new BaseKnownException("tip修复并发获取锁失败！");
+            }
+            ConditionsCommon con = new ConditionsCommon();
+            con.addEqParam("tipId", config.getTipId());
+            con.addEqParam("titleId", config.getTitleId());
+            List<BaseTipConfig> ll = baseTipConfigDao.queryColumnForList(con);
+            if (CollectionUtils.isEmpty(ll)) {
+                baseTipConfigDao.save(config);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
-    
-    
-    public List<LabelNameVO> getAllTips(){
-    	List<Object[]> listobj=baseTipJpaRepository.queryAllTip();
-    	if(!CollectionUtils.isEmpty(listobj)) {
-    		return listobj.stream().map(n->{
-    			LabelNameVO vo = new LabelNameVO();
-    			vo.setId(Long.parseLong(String.valueOf(n[0])));
-    			vo.setName(String.valueOf(n[1]));
-    			vo.setNum(Integer.parseInt(String.valueOf(n[2])));
-    			return vo;
-    		}).collect(Collectors.toList());
-    	}
-    	return Collections.EMPTY_LIST;
+
+
+    public List<LabelNameVO> getAllTips() {
+        List<Object[]> listobj = baseTipJpaRepository.queryAllTip();
+        if (!CollectionUtils.isEmpty(listobj)) {
+            return listobj.stream().map(n -> {
+                LabelNameVO vo = new LabelNameVO();
+                vo.setId(Long.parseLong(String.valueOf(n[0])));
+                vo.setName(String.valueOf(n[1]));
+                vo.setNum(Integer.parseInt(String.valueOf(n[2])));
+                return vo;
+            }).collect(Collectors.toList());
+        }
+        return Collections.EMPTY_LIST;
     }
-    
+
     @SuppressWarnings("unchecked")
-	public List<String> getTipsByTitleId(Long titleId) {
-    	ConditionsCommon condition = new ConditionsCommon();
-    	condition.addEqParam("titleId", titleId);
-    	List<BaseTipConfig> configList = baseTipConfigDao.queryColumnForList(condition);
-    	if(CollectionUtils.isEmpty(configList)) {
-    		return Collections.EMPTY_LIST;
-    	}
-    	return configList.stream().map(n->{
-    		BaseTip tip = baseTipDao.queryTById(n.getTipId());
-    		if(tip!=null) {
-    			return tip.getTipName();
-    		}else {
-    			return null;
-    		}
-    	}).collect(Collectors.toList());
+    public List<String> getTipsByTitleId(Long titleId) {
+        ConditionsCommon condition = new ConditionsCommon();
+        condition.addEqParam("titleId", titleId);
+        List<BaseTipConfig> configList = baseTipConfigDao.queryColumnForList(condition);
+        if (CollectionUtils.isEmpty(configList)) {
+            return Collections.EMPTY_LIST;
+        }
+        return configList.stream().map(n -> {
+            BaseTip tip = baseTipDao.queryTById(n.getTipId());
+            if (tip != null) {
+                return tip.getTipName();
+            } else {
+                return null;
+            }
+        }).collect(Collectors.toList());
     }
-    
-    public List<String> getTopTips(){
-    	List<String> tipList = baseTipJpaRepository.queryTopTip();
-    	return tipList;
+
+    public List<String> getTopTips() {
+        List<String> tipList = baseTipJpaRepository.queryTopTip();
+        return tipList;
     }
 }
--- general-basis/src/main/java/com/njq/basis/service/impl/BaseTypeService.java
+++ general-basis/src/main/java/com/njq/basis/service/impl/BaseTypeService.java
@@ -2,6 +2,9 @@ package com.njq.basis.service.impl;
 
 import com.njq.common.base.dao.ConditionsCommon;
 import com.njq.common.base.dao.DaoCommon;
+import com.njq.common.base.exception.BaseKnownException;
+import com.njq.common.base.redis.lock.JedisLock;
+import com.njq.common.base.redis.lock.JedisLockFactory;
 import com.njq.common.model.dao.BaseTypeNumJpaRepository;
 import com.njq.common.model.po.BaseType;
 import com.njq.common.model.po.BaseTypeNum;
@@ -22,6 +25,8 @@ public class BaseTypeService {
     private DaoCommon<BaseTypeNum> baseTypeNumDao;
     @Resource
     private BaseTypeNumJpaRepository baseTypeNumJpaRepository;
+    @Resource
+    private JedisLockFactory jedisLockFactory;
 
     /**
      * 校验和保存
@@ -33,16 +38,24 @@ public class BaseTypeService {
         if (StringUtil2.isEmpty(typeName)) {
             return null;
         }
-        ConditionsCommon condition = new ConditionsCommon();
-        condition.addEqParam("name", typeName.trim());
-        BaseType type = baseTypeDao.queryTByParamForOne(condition);
-        if (type == null) {
-            type = new BaseType();
-            type.setCreateDate(new Date());
-            type.setName(typeName.trim());
-            baseTypeDao.save(type);
+        String lockKey = StringUtil2.format("checkAndSave-{0}", typeName);
+        try (JedisLock jedisLock = this.jedisLockFactory.getLock(lockKey)) {
+            if (!jedisLock.acquire()) {
+                throw new BaseKnownException("type保存并发获取锁失败！");
+            }
+            ConditionsCommon condition = new ConditionsCommon();
+            condition.addEqParam("name", typeName.trim());
+            BaseType type = baseTypeDao.queryTByParamForOne(condition);
+            if (type == null) {
+                type = new BaseType();
+                type.setCreateDate(new Date());
+                type.setName(typeName.trim());
+                baseTypeDao.save(type);
+            }
+            return type.getId();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
         }
-        return type.getId();
     }
 
     /**
@@ -52,27 +65,36 @@ public class BaseTypeService {
      * @param typeId
      */
     public void addNum(String channel, Long typeId) {
-        ConditionsCommon condition = new ConditionsCommon();
-        condition.addEqParam("typeId", typeId);
-        condition.addEqParam("channel", channel);
-        BaseTypeNum num = baseTypeNumDao.queryTByParamForOne(condition);
-        if (num == null) {
-            num = new BaseTypeNum();
-            num.setCreateDate(new Date());
-            num.setTypeId(typeId);
-            num.setNum(1);
-            num.setChannel(channel);
-            baseTypeNumDao.save(num);
-        } else {
-            baseTypeNumJpaRepository.updateForAddNum(channel, typeId);
+        String lockKey = StringUtil2.format("typeaddNum-{0}-{1}", typeId, channel);
+        try (JedisLock jedisLock = this.jedisLockFactory.getLock(lockKey)) {
+            if (!jedisLock.acquire()) {
+                throw new BaseKnownException("type添加数量并发获取锁失败！");
+            }
+            ConditionsCommon condition = new ConditionsCommon();
+            condition.addEqParam("typeId", typeId);
+            condition.addEqParam("channel", channel);
+            BaseTypeNum num = baseTypeNumDao.queryTByParamForOne(condition);
+            if (num == null) {
+                num = new BaseTypeNum();
+                num.setCreateDate(new Date());
+                num.setTypeId(typeId);
+                num.setNum(1);
+                num.setChannel(channel);
+                baseTypeNumDao.save(num);
+            } else {
+                baseTypeNumJpaRepository.updateForAddNum(channel, typeId);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
         }
+
     }
 
     public BaseType getTypeById(Long id) {
         return baseTypeDao.queryTById(id);
     }
-    
-    public List<LabelNameVO> getAllTypes(){
-    	return baseTypeNumJpaRepository.queryAllType();
+
+    public List<LabelNameVO> getAllTypes() {
+        return baseTypeNumJpaRepository.queryAllType();
     }
 }
